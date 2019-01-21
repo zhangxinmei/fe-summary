531f738ac4f27fd44b39d840f029eef166bed2ae
may_xmzhang <may_xmzhang@vipabc.com>
2019-01-21 14:23:59 +0800

    update:doc

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  base: "/fe-summary/",
+  title: "",
+  description: "",
+  ga: "UA-132397402-1",
+  head: [
+    ["link", { rel: "icon", href: `/logo.png` }],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" }
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#000000" }]
+  ]
+};
